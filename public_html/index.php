962689b5c45aecab704ecbdfe3b8672883c9c347
Christon-Khong <christon.khong@gmail.com>
2020-01-17 11:28:05 -0700

    Assignment - Create a working site

---
+++ public_html/index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Christon's CSIS 2440 Homepage</title>
+    <?php
+
+    ?>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"><img class="img-fluid rounded mx-auto d-block my-picture" src="https://media-exp1.licdn.com/dms/image/C4E03AQG8OCFAsMW17Q/profile-displayphoto-shrink_200_200/0?e=1584576000&v=beta&t=dIq1hRrI9_G3sWHv7Pg2eD16N44StxLI6lDkVM70zF8" alt="Christon"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="https://csis-2440-ckhong.000webhostapp.com/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">E-commerce Site</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Assignments
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Assignment 1</a>
+                        <a class="dropdown-item" href="#">Assignment 2</a>
+                        <a class="dropdown-item" href="#">Assignment 3</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Classroom Exercises
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="https://csis-2440-ckhong.000webhostapp.com/ClassroomExercises/CE01ckhong.php">CE 01</a>
+                        <a class="dropdown-item" href="#">CE 02</a>
+                        <a class="dropdown-item" href="#">CE 03</a>
+                        <a class="dropdown-item" href="#">CE 04</a>
+                        <a class="dropdown-item" href="#">CE 05</a>
+                        <a class="dropdown-item" href="#">CE 06</a>
+                        <a class="dropdown-item" href="#">CE 07</a>
+                        <a class="dropdown-item" href="#">CE 08</a>
+                        <a class="dropdown-item" href="#">CE 09</a>
+                        <a class="dropdown-item" href="#">CE 10</a>
+                        <a class="dropdown-item" href="#">CE 11</a>
+                        <a class="dropdown-item" href="#">CE 12</a>
+                        <a class="dropdown-item" href="#">CE 13</a>
+                        <a class="dropdown-item" href="#">CE 14</a>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+    <div class="d-flex flex-column text-light">
+        <div>
+            <h2 class="m-3">About Me</h2>
+            <div class="d-flex flex-row flex-wrap">
+                <p class="m-3 mx-xs-auto about-me">I enjoy playing games and spending time with family. I love travelling with my wife and exploring the local cuisine. My latest favorite book is "Tuesdays with Morrie" and I enjoy watching many movies including Lord of the Rings.</p>
+                <img class="img-fluid rounded m-auto lotr-pic" src="https://preview.redd.it/k7osr6zxg3821.jpg?width=960&crop=smart&auto=webp&s=ebbfb4897b5144b23c82cc6db12d20a35cd88bf8">
+            </div>
+        </div>
+    </div> <?php
+
+            ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
