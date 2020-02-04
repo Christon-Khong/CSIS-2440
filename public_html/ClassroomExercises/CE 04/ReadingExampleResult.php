e61f96cb4782c716707226af70b9f27d64588527
Christon Khong <christon.khong@gmail.com>
2020-02-04 11:10:41 -0700

    CE 04 Starter files

---
+++ public_html/ClassroomExercises/CE 04/ReadingExampleResult.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+//get post variable
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8">
+                        <?php
+                        // put your code here
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
